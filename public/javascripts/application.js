57756a2ee20ddcff4bca794ce91423359a3a2e66
Anurag Priyam <anurag08priyam@gmail.com>
2009-06-29 16:41:39 +0530

    random

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("ul#tabs > li").click(function(event){
+  $("ul#tabs li").click(function(event){
     $("div.tabbed div").addClass("hidden");
     $("div.tabbed div:eq(" + $('ul#tabs > li').index(this)  + ")").removeClass("hidden");
   });
